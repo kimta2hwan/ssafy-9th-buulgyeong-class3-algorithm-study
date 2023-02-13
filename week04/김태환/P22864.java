ab1e9cb8bc9da09501d1b2f3dc13b53c5abad52c
TaeHwan <kimta2hwan@gmail.com>
2023-02-13 19:59:03 +0900

    김태환: 22864 피로도

---
+++ week04/김태환/P22864.java
@@ -0,0 +1,42 @@
+class P22864 {
+	public static void main(String[] args) throws Exception {
+		int A = read();
+		int B = read();
+		int C = read();
+		int M = read();
+		
+		int t = 0;
+		int a = 0;
+		int b = 0;
+		
+		while (t++ < 24) {
+			if (a + A > M) {
+				a -= C;
+				
+				if (a < 0) {
+					a = 0;
+				}
+				
+			} else {
+				a += A;
+				b += B;
+			}
+		}
+		
+		System.out.println(b);
+	}
+	
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		
+		while ((c = System.in.read()) > 32) {
+			n = (n << 3) + (n << 1) + (c & 15);
+		}
+
+        if (c == 13) {
+            System.in.read();
+        }
+		
+		return n;
+	}
+}
